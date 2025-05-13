b7ce80c9cf1cb354b80425b046cdcc7cd872a26c
Alwayne Bailey <alwayne.bailey@reprisk.com>
2025-05-13 15:43:21 -0400

    fix api key issue

--- Frontend/react-app/src/utils/youtubeAPI.ts
+++ Frontend/react-app/src/utils/youtubeAPI.ts
@@ -1,19 +1,18 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY || "AIzaSyBT0HmgQjffNN-dDnqlB_nuOOwaWiR7sRQ"
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 async function fetchYouTubeVideoDetails(videoId: string) {
-    try {
-        const res = await axios.get(
-            `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`
-        );
-        if (!res.data.items.length) throw new Error("No video found.");
-        return res.data.items[0];
-    } catch (err: any) {
-        console.error("YouTube Error:", err);
-        throw new Error("Failed to fetch YouTube metadata.");
-    }
-};
-
+  try {
+    const res = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`
+    );
+    if (!res.data.items.length) throw new Error("No video found.");
+    return res.data.items[0];
+  } catch (err: any) {
+    console.error("YouTube Error:", err);
+    throw new Error("Failed to fetch YouTube metadata.");
+  }
+}
 
 export { fetchYouTubeVideoDetails };
